2190512b3f14949ca3720f8b69448ae2e04a7095
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2018-05-13 18:13:28 -0400

    Simple implementation for reactions on paginators

--- src/main/kotlin/xyz/astolfo/astolfocommunity/Menu.kt
+++ src/main/kotlin/xyz/astolfo/astolfocommunity/Menu.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import net.dv8tion.jda.core.entities.Message
+import net.dv8tion.jda.core.events.message.react.GenericMessageReactionEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
 import kotlin.math.max
 import kotlin.math.min
 
@@ -35,10 +37,27 @@ class PaginatorBuilder(private val commandExecution: CommandExecution, var title
     fun build() = Paginator(commandExecution, titleProvider, provider, renderer)
 }
 
-class PaginatorProvider(val perPage: Int, val provider: () -> List<String>)
+class PaginatorProvider(val perPage: Int, provider: () -> List<String>) {
+    val provider = {
+        val provided = provider.invoke()
+        pageCount = Math.ceil(provided.size.toDouble() / perPage).toInt()
+        provided
+    }
+    var pageCount: Int = -1
+        get() {
+            if (field == -1) provider.invoke()
+            return field
+        }
+        private set
+}
 
 class Paginator(private val commandExecution: CommandExecution, val titleProvider: () -> String?, val provider: PaginatorProvider, val renderer: Paginator.() -> Message) {
     var currentPage = 0
+        set(value) {
+            val oldPage = field
+            field = max(0, min(value, provider.pageCount - 1))
+            if (field != oldPage) render()
+        }
 
     val providedContent: List<String>
         get() {
@@ -54,12 +73,47 @@ class Paginator(private val commandExecution: CommandExecution, val titleProvide
 
     private var message: Deferred<Message>? = null
 
-    init {
-        render()
-        commandExecution.session().addDestroyListener {
-            // Clean Up
-            launch { message?.await()?.delete()?.queue() }
+    private val listener = object : ListenerAdapter() {
+        override fun onGenericMessageReaction(event: GenericMessageReactionEvent?) {
+            if (event!!.user.idLong != commandExecution.event.author.idLong) return
+            if (message!!.isActive || message!!.getCompleted().idLong != event.messageIdLong) return
+            val name = event.reactionEmote.name
+            if (name == SELECT) {
+                destroy()
+            } else {
+                val newPage = when (name) {
+                    START_ARROW -> 0
+                    BACK_ARROW -> currentPage - 1
+                    FORWARD_ARROW -> currentPage + 1
+                    END_ARROW -> provider.pageCount
+                    else -> -1
+                }
+                currentPage = newPage
+            }
         }
+    }
+
+    val destroyListener = { destroy() }
+
+    init {
+        commandExecution.event.jda.addEventListener(listener)
+        render()
+        commandExecution.session().addDestroyListener(destroyListener)
+    }
+
+    fun destroy() {
+        // Clean Up
+        commandExecution.session().removeDestroyListener(destroyListener)
+        commandExecution.event.jda.removeEventListener(listener)
+        launch { message?.await()?.delete()?.queue() }
+    }
+
+    companion object {
+        val START_ARROW = "\u23EE"
+        val BACK_ARROW = "\u25C0"
+        val SELECT = "\u23F9"
+        val FORWARD_ARROW = "\u25B6"
+        val END_ARROW = "\u23ED"
     }
 
     fun render() {
@@ -67,7 +121,18 @@ class Paginator(private val commandExecution: CommandExecution, val titleProvide
         message = async {
             val currentMessage = lastMessage?.await()
             val newMessage = renderer.invoke(this@Paginator)
-            if (currentMessage == null) commandExecution.messageAction(newMessage).complete()
+            if (currentMessage == null) commandExecution.messageAction(newMessage).complete().apply {
+
+                val pageCount = provider.pageCount
+                val emotes = mutableListOf<String>()
+                if (pageCount > 2) emotes.add(START_ARROW)
+                if (pageCount > 1) emotes.add(BACK_ARROW)
+                emotes.add(SELECT)
+                if (pageCount > 1) emotes.add(FORWARD_ARROW)
+                if (pageCount > 2) emotes.add(END_ARROW)
+
+                emotes.forEach { addReaction(it).queue() }
+            }
             else currentMessage.editMessage(newMessage).complete()
         }
     }
